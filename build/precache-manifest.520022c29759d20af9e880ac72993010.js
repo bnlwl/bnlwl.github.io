343650b9e821c1773955ac3ea656a60930e934b4
bnlwl <361628020@qq.com>
2019-08-06 15:25:23 +0800

    feat:更新简历

---
+++ build/precache-manifest.520022c29759d20af9e880ac72993010.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df0892a3775e1702710b7883537beb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae757c0e9a6f7255c2c5",
+    "url": "./static/css/main.1b224d50.chunk.css"
+  },
+  {
+    "revision": "e1c2bea85f92a15af2bd",
+    "url": "./static/js/2.602e5d28.chunk.js"
+  },
+  {
+    "revision": "ae757c0e9a6f7255c2c5",
+    "url": "./static/js/main.987bc1d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "./static/js/runtime~main.a8a9905a.js"
+  }
+]);
