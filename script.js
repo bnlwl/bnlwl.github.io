dff174f92a5bb6102b0f61177abec95a8d1a4916
bnlwl <bnlwl@sina.com>
2023-08-25 18:24:02 +0800

    feat: 添加重置功能

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const boardSize = 15;
-  const board = [];
-  const playerX = 'x';
-  const playerO = 'o';
-  let currentPlayer = playerX;
+const boardSize = 15;
+const board = [];
+const playerX = 'blue';
+const playerO = 'red';
+let currentPlayer = playerX;
 
+// 重置游戏
+function resetGame() {
+  for (let row = 0; row < boardSize; row++) {
+    for (let col = 0; col < boardSize; col++) {
+      board[row][col] = ' ';
+      const cell = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+      cell.classList = ['cell']
+    }
+  }
+  currentPlayer = playerX;
+}
+
+// 判断是否有玩家胜利
+function checkWin(row, col) {
+  const directions = [
+    [1, 0], [0, 1], [1, 1], [-1, 1]
+  ];
+  for (const [dr, dc] of directions) {
+    let count = 1;
+    for (let i = 1; i < 5; i++) {
+      const r = row + i * dr;
+      const c = col + i * dc;
+      if (r >= 0 && r < boardSize && c >= 0 && c < boardSize && board[r][c] === board[row][col]) {
+        count++;
+      } else {
+        break;
+      }
+    }
+    for (let i = 1; i < 5; i++) {
+      const r = row - i * dr;
+      const c = col - i * dc;
+      if (r >= 0 && r < boardSize && c >= 0 && c < boardSize && board[r][c] === board[row][col]) {
+        count++;
+      } else {
+        break;
+      }
+    }
+    if (count >= 5) {
+      return true;
+    }
+  }
+  return false;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
   const boardElement = document.getElementById('board');
 
   // 初始化棋盘
@@ -42,48 +86,5 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   });
-
-  // 判断是否有玩家胜利
-  function checkWin(row, col) {
-    const directions = [
-      [1, 0], [0, 1], [1, 1], [-1, 1]
-    ];
-    for (const [dr, dc] of directions) {
-      let count = 1;
-      for (let i = 1; i < 5; i++) {
-        const r = row + i * dr;
-        const c = col + i * dc;
-        if (r >= 0 && r < boardSize && c >= 0 && c < boardSize && board[r][c] === board[row][col]) {
-          count++;
-        } else {
-          break;
-        }
-      }
-      for (let i = 1; i < 5; i++) {
-        const r = row - i * dr;
-        const c = col - i * dc;
-        if (r >= 0 && r < boardSize && c >= 0 && c < boardSize && board[r][c] === board[row][col]) {
-          count++;
-        } else {
-          break;
-        }
-      }
-      if (count >= 5) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  // 重置游戏
-  function resetGame() {
-    for (let row = 0; row < boardSize; row++) {
-      for (let col = 0; col < boardSize; col++) {
-        board[row][col] = ' ';
-        const cell = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
-        cell.classList = ['cell']
-      }
-    }
-    currentPlayer = playerX;
-  }
 });
+
